bbb3184351411bef915b5156bb883f622866a947
foxten <tenaysiafox@gmail.com>
2020-07-05 18:24:11 -0500

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,17 @@
+require 'pry'
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+    def full_name
+        "#{first_name} #{last_name}"
+    end
+
+    def list_roles
+        characters_and_shows = []
+        search = Character.where(actor_id: self.id) 
+        search.map{|info| "#{info.name} - #{info.show.name}"}
+    end
 end
+
+# binding.pry
